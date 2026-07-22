98063bbbe79be0f60eb3beaea96c0d8cad48286c
Deepak Kumar Thakur <dk1816859@gmail.com>
2026-07-23 00:24:26 +0530

    Add cloudinary dependency and implement pagination in getHackathons controller

--- HackSphere-backend/src/controllers/hackathon/getHackathons.js
+++ HackSphere-backend/src/controllers/hackathon/getHackathons.js
@@ -3,9 +3,15 @@ import Hackathon from "../../models/Hackathon.js";
 export const getHackathons = async (req, res) => {
     try {
 
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const skip = (page - 1) * limit;
+
         const hackathons = await Hackathon.find({
             status: "published",
-        }).populate("createdBy", "firstName lastName email").sort({ createdAt: -1 });
+        }).populate("createdBy", "firstName lastName email").sort({ createdAt: -1 }).skip(skip).limit(limit);
+
+        
 
         return res.status(200).json({
             success: true,
