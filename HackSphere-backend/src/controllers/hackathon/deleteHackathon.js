1119ee4f31660ef773e7a93838b472f9b142f7e9
Deepak Kumar Thakur <dk1816859@gmail.com>
2026-07-25 11:41:00 +0530

    Fix deleteHackathon controller to validate ID and handle responses correctly

--- HackSphere-backend/src/controllers/hackathon/deleteHackathon.js
+++ HackSphere-backend/src/controllers/hackathon/deleteHackathon.js
@@ -3,6 +3,26 @@ import Hackathon from "../../models/Hackathon.js";
 
 export const deleteHackathon = async (req,res) =>{
     try{
+        const { id } = req.params;
+         if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid Hackathon ID",
+            });
+        }
+        const hackathon = await Hackathon.deleteOne(id).populate("DeleatedBy", "firstName lastName email");
+        if (!hackathon) {
+            return res.status(404).json({
+                success: false,
+                message: "Hackathon not found",
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Hackathon Deleteed successfully",
+            data: hackathon,
+        });
 
     }
     catch(error){
